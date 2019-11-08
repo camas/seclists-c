6905436b0372368b3c82f3a9d53b7ae186dc3698
Camas <camas@hotmail.co.uk>
2019-11-08 01:35:19 +0000

    Fix lists that don't end in a newline

--- c-extensions/check_blank_ends.sh
+++ c-extensions/check_blank_ends.sh
@@ -9,12 +9,15 @@ if [[ $(git rev-parse --show-toplevel) != "$PWD" ]]; then
     exit 1
 fi
 
+# Find all text files
 text_files=(**/*.txt)
 echo "Checking ${#text_files[@]} files" 
 
+# Check each one
 for file in "${text_files[@]}"; do
     blank=$(tail -c1 "$file")
     if [[ "$blank" != $'' ]]; then
+        # Print file name and last 16 chars
         echo "$file has non-blank last line"
         echo "    $(tail -c16 "$file" | xxd)"
     fi
